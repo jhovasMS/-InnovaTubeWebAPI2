750f2b0deaefc1ae02651f0635fa16f5adb6dbbd
jhovasMS <jhovany2244@gmail.com>
2025-05-20 05:58:34 -0600

    Proyecto corregido por no generar Docker de inicio

---
+++ InnovaTubeWebAPI/Program.cs
@@ -0,0 +1,36 @@
+using InnovaTubeWebAPI.Datos;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+var origenesPermitidos = builder.Configuration.GetValue<string>("OrigenesPermitidos")!.Split(",");
+
+builder.Services.AddCors(opciones =>
+{
+    opciones.AddDefaultPolicy(politica =>
+    {
+        politica.WithOrigins(origenesPermitidos).AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
+var connectionStrings = builder.Configuration.GetValue<string>("ConnectionStrings");
+builder.Services.AddDbContext<ApplicationDbContext>(opciones =>
+    opciones.UseNpgsql("name=DefaultConnection"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseCors();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
